c7cf37b5c131b7de66ad74e6bfb38ce71eefcd69
gopisivanappan <gopisivanappan@outlook.com>
2022-08-03 11:09:35 +0000

    components added for the page.

---
+++ SitecoreCDPSln/src/Feature/Components/code/Controllers/CodeContentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitecoreCDP.Feature.Components.Controllers
+{
+    public class CodeContentController : Controller
+    {
+        // GET: CodeContent
+        public ActionResult Index()
+        {
+            return View("/Views/CodeContent/Index.cshtml");
+        }
+    }
+}
---
+++ SitecoreCDPSln/src/Feature/Components/code/Controllers/TabContentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitecoreCDP.Feature.Components.Controllers
+{
+    public class TabContentController : Controller
+    {
+        // GET: TabContent
+        public ActionResult Index()
+        {
+            return View("/Views/TabContent/Index.cshtml");
+        }
+    }
+}
