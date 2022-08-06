c37bc6391884bcf48c5f0fed73921c06b21841e0
gopisivanappan <gopisivanappan@outlook.com>
2022-08-06 04:46:14 +0000

    preparation for day 2

---
+++ SitecoreCDPSln/src/Feature/Components/code/Controllers/PurchasePathController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitecoreCDP.Feature.Components.Controllers
+{
+    public class PurchasePathController : Controller
+    {
+        // GET: PurchasePath
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
