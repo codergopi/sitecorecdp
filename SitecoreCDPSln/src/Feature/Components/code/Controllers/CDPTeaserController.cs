eb6b569c82ce9f9f4529a4b47153eb5a3755f9e7
gopisivanappan <gopisivanappan@outlook.com>
2022-08-22 20:06:41 +0000

    Content and Recent Changes Committed

---
+++ SitecoreCDPSln/src/Feature/Components/code/Controllers/CDPTeaserController.cs
@@ -0,0 +1,35 @@
+﻿using Sitecore.Data.Fields;
+using Sitecore.Links;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Web.UI.WebControls;
+using SitecoreCDP.Feature.Components.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitecoreCDP.Feature.Components.Controllers
+{
+    public class CDPTeaserController : Controller
+    {
+        // GET: CDPTeaser
+        public ActionResult Index()
+        {
+            var renderingItem = RenderingContext.Current.Rendering.Item;
+            MultilistField productTags = renderingItem.Fields["type"];
+            var productTagsString = productTags.GetItems().Select(x => x.Name).ToList();
+            DateField updatedDateTime = renderingItem.Fields["__UpdatedDateTime"];
+
+            ProductCard productCard = new ProductCard
+            {
+                ProdTitle = renderingItem.Fields["__DisplayName"].Value,
+                ProdUrl = LinkManager.GetItemUrl(renderingItem),
+                ProdImage = new HtmlString(FieldRenderer.Render(renderingItem, "image")),
+                ProdTags = productTagsString,
+            };
+
+            return View(productCard);
+        }
+    }
+}
---
+++ SitecoreCDPSln/src/Feature/Components/code/Models/ProductCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreCDP.Feature.Components.Models
+{
+    public class ProductCard
+    {
+        public string ProdTitle { get; set; }
+        public string ProdUrl { get; set; }
+        public HtmlString ProdImage { get; set; }
+        public List<string> ProdTags { get; set; }
+        public string LastUpdated { get; set; }
+    }
+}
